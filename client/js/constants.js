4bfad3cc1d87092111558eef533ca2091874a2f5
mytusshar <mytusshar@gmail.com>
2018-04-16 17:09:47 +0530

    Updating Client

--- client/js/constants.js
+++ client/js/constants.js
@@ -3,15 +3,15 @@
  * author: Tushar Bochare
  * Email: mytusshar@gmail.com
  */
-// const CLIENT_REDIRECT_URL = "http://localhost:8080/client/index.html";
-const CLIENT_REDIRECT_URL = "http://envclient-env.us-east-1.elasticbeanstalk.com/index.html";
+const CLIENT_REDIRECT_URL = "http://localhost:8080/client/index.html";
+// const CLIENT_REDIRECT_URL = "http://envclient-env.us-east-1.elasticbeanstalk.com/index.html";
 
 const AWS_REGION = "us-east-1";
 const AWS_ENDPOINT = "http://dynamodb.us-east-1.amazonaws.com";
 const TABLE_NAME = "users";
 
-// const SERVER_ADDRESS = "http://localhost:8081";
-const SERVER_ADDRESS = "https://iamauthmodule-env.us-east-1.elasticbeanstalk.com";
+const SERVER_ADDRESS = "http://localhost:8081";
+// const SERVER_ADDRESS = "https://iamauthmodule-env.us-east-1.elasticbeanstalk.com";
 
 const URL_AUTHENTICATION = SERVER_ADDRESS + "/auth";
 const REFRESH_URL = SERVER_ADDRESS + "/refresh";
